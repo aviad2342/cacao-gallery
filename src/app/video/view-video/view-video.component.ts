ac1615d9c62ae883f9d07e2e7dda8b9eed34c5ce
Aviad <aviad2342@walla.com>
2021-05-14 21:11:08 +0300

    Video Delete Dialog

--- src/app/video/view-video/view-video.component.ts
+++ src/app/video/view-video/view-video.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { Video } from '../video.model';
+import { VideoService } from '../video.service';
 
 @Component({
   selector: 'app-view-video',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewVideoComponent implements OnInit {
 
-  constructor() { }
+  video: Video;
+  activeUrl = '';
+  isValidUrl = true;
+  isLoading = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private videoService: VideoService,
+    private appService: AppService
+    ) { }
 
   ngOnInit(): void {
+
+    this.activeUrl = this.router.url;
+    this.isLoading = true;
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('id')) {
+        this.location.back();
+        return;
+      }
+      this.videoService.getVideo(paramMap.get('id')).subscribe(video => {
+            this.video = video;
+            this.isLoading = false;
+          },
+          error => {
+            this.isLoading = false;
+            if (this.router.isActive(this.activeUrl, false)) {
+              this.appService.presentToast('ישנה תקלה! לא ניתן להציג את הסרטון.', false);
+              this.location.back();
+            }
+          }
+        );
+    });
+
+    this.activeUrl = this.router.url;
   }
 
 }
